f91f1add18b28511420ff6602a3e40fe992cd5ce
PikoloGL <39061042+PikoloGL@users.noreply.github.com>
2026-03-02 22:51:24 +0200

    Delete tests/epam-client-work.spec.ts

--- tests/epam-client-work.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.describe('EPAM Client Work navigation', () => {
-  test('navigates to Client Work via Services menu', async ({ page }) => {
-    // TODO: implement test steps
-  });
-});
